398da810069a58e153ed904921a36b95d437cd42
andreslag1-jpg <andres.lag1@gmail.com>
2025-08-15 20:25:05 -0500

    Update reportes.py

--- reportes.py
+++ reportes.py
@@ -1,4 +1,3 @@
-# reportes.py
 from typing import List, Tuple
 from db import get_conn
 
@@ -6,8 +5,7 @@ class Reportes:
     def inventario_detallado(self) -> List[Tuple]:
         sql = """
         SELECT p.id_producto, p.nombre, p.descripcion, p.precio,
-               i.cantidad, i.ubicacion_bodega,
-               (p.precio * i.cantidad) AS valor_total
+               i.cantidad, i.ubicacion_bodega
         FROM producto p
         JOIN inventario i ON p.id_producto = i.id_producto
         ORDER BY i.ubicacion_bodega, p.nombre;
@@ -18,10 +16,11 @@ class Reportes:
 
     def resumen_por_bodega(self) -> List[Tuple]:
         sql = """
-        SELECT i.ubicacion_bodega,
-               COUNT(DISTINCT p.id_producto) AS productos_diferentes,
-               SUM(i.cantidad) AS total_unidades,
-               SUM(p.precio * i.cantidad) AS valor_total_bodega
+        SELECT
+            i.ubicacion_bodega,
+            COUNT(DISTINCT p.id_producto) AS productos_diferentes,
+            SUM(i.cantidad) AS total_unidades,
+            SUM(p.precio * i.cantidad) AS valor_total
         FROM producto p
         JOIN inventario i ON p.id_producto = i.id_producto
         GROUP BY i.ubicacion_bodega
