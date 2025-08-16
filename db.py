7aa88f39344f4ae8af5432c2aaaac32762b6960e
andreslag1-jpg <andres.lag1@gmail.com>
2025-08-15 20:13:46 -0500

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,12 @@ import mysql.connector as mysql
 
 DB_CFG = {
     "host": "localhost",
-    "user": "root",            # Usuario MySQL
-    "password": "123456", # <-- cámbialo por tu clave
+    "user": "root",           
+    "password": "CAMBIAR_EN_LOCAL",
     "database": "tienda_online"
 }
 
 def get_conn():
     """Devuelve una conexión a la base de datos MySQL."""
     return mysql.connect(**DB_CFG)
+
